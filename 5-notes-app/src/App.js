90d5e337250cc3ef7dba5a0aec287e3851431e55
Pablo Pedraza <wensel84@gmail.com>
2022-04-22 16:30:46 -0300

    agrego login

--- 5-notes-app/src/App.js
+++ 5-notes-app/src/App.js
@@ -1,17 +1,38 @@
 import { useState, useEffect } from 'react'
 import NoteForm from './components/NoteForm'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
+import loginService from './services/login'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes)
     })
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogout = () => {
+    setUser(null)
+    noteService.setToken(user.token)
+    window.localStorage.removeItem('loggedNoteAppUser')
+  }
 
   const addNote = (noteObject) => {
     noteService.create(noteObject).then((returnedNote) => {
@@ -35,11 +56,66 @@ const App = () => {
       .then((returnedNote) =>
         setNotes(notes.map((n) => (n.id === id ? returnedNote : n)))
       )
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter((n) => n.id !== id))
+      })
+  }
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault()
+    console.log('submitt')
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      console.log(user)
+
+      window.localStorage.setItem('loggedNoteAppUser', JSON.stringify(user))
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
   return (
     <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLoginSubmit}>
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          placeholder="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          placeholder="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Login</button>
+      </form>
+
+      <Notification message={errorMessage} />
       <NoteForm addNote={addNote}></NoteForm>
+      <div>
+        <button onClick={handleLogout}>Cerrar Sesion</button>
+      </div>
       <h1>Notas</h1>
       <button onClick={handleShowAll}>
         {showAll ? 'Muestra solo importantes' : 'Muestra Todos'}
--- 5-notes-app/src/services/notes.js
+++ 5-notes-app/src/services/notes.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/notes'
 
+let token = null
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+}
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then((response) => {
@@ -10,7 +14,13 @@ const getAll = () => {
 }
 
 const create = (newObject) => {
-  const request = axios.post(baseUrl, newObject)
+  const config = {
+    headers: {
+      Authorization: token,
+    },
+  }
+
+  const request = axios.post(baseUrl, newObject, config)
   return request.then((response) => response.data)
 }
 
@@ -19,6 +29,6 @@ const update = (id, newObject) => {
   return request.then((response) => response.data)
 }
 
-const notes = { getAll, create, update }
+const notes = { getAll, create, update, setToken }
 
 export default notes
